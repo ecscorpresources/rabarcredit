d5be9cfb79dd4fce20bccdd77426b0a67b45aa49
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2020-02-04 18:22:44 +0100

    added Navbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,14 +3,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "../includes/Footer";
-import Pagenav from "../includes/Pagenav";
+import Navbar from "../includes/Navbar/Navbar";
 import Homepage from "../pages/Homepage/Homepage";
 
 function App() {
   return (
     <>
       <Router>
-        <Pagenav />
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Homepage} />
         </Switch>
---
+++ src/components/includes/Navbar/Toolbar/Toolbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Toolbar.css";
+import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
+
+const Toolbar = props => {
+  return (
+    <header className="toolbar">
+      <nav className="toolbar_navigation">
+        <div className="toolbar_toggle_button">
+          <DrawerToggleButton
+            handleDrawerToggleClick={props.handleDrawerToggleClick}
+          />
+        </div>
+        <div className="toolbar_logo">
+          <img
+            src={require("../../../../assets/img/Rabar Credit.png")}
+            alt=""
+          />
+        </div>
+        {/* <div className="spacer"></div> */}
+        <div className="toolbar_navigation_items">
+          <ul className="links">
+            <li>
+              <Link to="/">How it works</Link>
+            </li>
+            <li>
+              <Link to="/">Products</Link>
+            </li>
+            <li>
+              <Link to="/">Pay Back loan</Link>
+            </li>
+            <li>
+              <Link to="/">Let's Talk</Link>
+            </li>
+          </ul>
+          <ul className="login_signup">
+            <li>
+              <Link to="/">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/">Log In</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Toolbar;
